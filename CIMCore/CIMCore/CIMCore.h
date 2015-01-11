a8c5eada72df3579759889d7adefac9d6cd787a2
Charles <i_charles@foxmail.com>
2015-01-11 20:39:23 +0800

    CIMCore Dynamic Framework

---
+++ CIMCore/CIMCore/CIMCore.h
@@ -0,0 +1,19 @@
+//
+//  CIMCore.h
+//  CIMCore
+//
+//  Created by Charles on 15/1/11.
+//  Copyright (c) 2015年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CIMCore.
+FOUNDATION_EXPORT double CIMCoreVersionNumber;
+
+//! Project version string for CIMCore.
+FOUNDATION_EXPORT const unsigned char CIMCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CIMCore/PublicHeader.h>
+
+
